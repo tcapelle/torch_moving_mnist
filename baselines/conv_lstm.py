71f4c776c1189285b4db4da8d00d94ef72320c40
soumik12345 <19soumik.rakshit96@gmail.com>
2023-01-11 14:55:44 +0000

    add: conv lstm baseline [WIP]

---
+++ baselines/conv_lstm.py
@@ -0,0 +1,151 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch_moving_mnist.data import MovingMNIST
+from torch_moving_mnist.utils import show_images
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class ConvLSTMCell(nn.Module):
+    def __init__(
+        self, in_channels, out_channels, kernel_size, padding, activation, frame_size
+    ):
+        super(ConvLSTMCell, self).__init__()
+
+        if activation == "tanh":
+            self.activation = torch.tanh
+        elif activation == "relu":
+            self.activation = torch.relu
+
+        self.conv = nn.Conv2d(
+            in_channels=in_channels + out_channels,
+            out_channels=4 * out_channels,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
+
+        self.W_ci = nn.Parameter(torch.Tensor(out_channels, *frame_size))
+        self.W_co = nn.Parameter(torch.Tensor(out_channels, *frame_size))
+        self.W_cf = nn.Parameter(torch.Tensor(out_channels, *frame_size))
+
+    def forward(self, X, H_prev, C_prev):
+        conv_output = self.conv(torch.cat([X, H_prev], dim=1))
+        i_conv, f_conv, C_conv, o_conv = torch.chunk(conv_output, chunks=4, dim=1)
+        input_gate = torch.sigmoid(i_conv + self.W_ci * C_prev)
+        forget_gate = torch.sigmoid(f_conv + self.W_cf * C_prev)
+        C = forget_gate * C_prev + input_gate * self.activation(C_conv)
+        output_gate = torch.sigmoid(o_conv + self.W_co * C)
+        H = output_gate * self.activation(C)
+        return H, C
+
+
+class ConvLSTM(nn.Module):
+    def __init__(
+        self, in_channels, out_channels, kernel_size, padding, activation, frame_size
+    ):
+        super(ConvLSTM, self).__init__()
+        self.out_channels = out_channels
+        self.convLSTMcell = ConvLSTMCell(
+            in_channels, out_channels, kernel_size, padding, activation, frame_size
+        )
+
+    def forward(self, X):
+        batch_size, _, seq_len, height, width = X.size()
+        output = torch.zeros(
+            batch_size, self.out_channels, seq_len, height, width, device=device
+        )
+        H = torch.zeros(batch_size, self.out_channels, height, width, device=device)
+        C = torch.zeros(batch_size, self.out_channels, height, width, device=device)
+        for time_step in range(seq_len):
+            H, C = self.convLSTMcell(X[:, :, time_step], H, C)
+            output[:, :, time_step] = H
+        return output
+
+
+class Seq2Seq(nn.Module):
+    def __init__(
+        self,
+        num_channels,
+        num_kernels,
+        kernel_size,
+        padding,
+        activation,
+        frame_size,
+        num_layers,
+    ):
+        super(Seq2Seq, self).__init__()
+        self.sequential = nn.Sequential()
+        self.sequential.add_module(
+            "convlstm1",
+            ConvLSTM(
+                in_channels=num_channels,
+                out_channels=num_kernels,
+                kernel_size=kernel_size,
+                padding=padding,
+                activation=activation,
+                frame_size=frame_size,
+            ),
+        )
+        self.sequential.add_module(
+            "batchnorm1", nn.BatchNorm3d(num_features=num_kernels)
+        )
+        for l in range(2, num_layers + 1):
+            self.sequential.add_module(
+                f"convlstm{l}",
+                ConvLSTM(
+                    in_channels=num_kernels,
+                    out_channels=num_kernels,
+                    kernel_size=kernel_size,
+                    padding=padding,
+                    activation=activation,
+                    frame_size=frame_size,
+                ),
+            )
+            self.sequential.add_module(
+                f"batchnorm{l}", nn.BatchNorm3d(num_features=num_kernels)
+            )
+
+        self.conv = nn.Conv2d(
+            in_channels=num_kernels,
+            out_channels=num_channels,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
+
+    def forward(self, X):
+        output = self.sequential(X)
+        output = self.conv(output[:, :, -1])
+        return nn.Sigmoid()(output)
+
+
+affine_params = dict(
+    angle=(-20, 20),  # rotation in degrees (min and max values)
+    translate=((-30, 30), (-30, 30)),  # translation in pixels x and y
+    scale=(0.8, 1.3),  # scaling in percentage (1.0 = no scaling)
+    shear=(-20, 20),  # deformation on the z-plane
+)
+dataset = MovingMNIST(
+    path=".",
+    affine_params=affine_params,
+    num_digits=[3],
+    num_frames=4,
+    img_size=64,
+    concat=True,
+    normalize=True,
+)
+
+
+model = Seq2Seq(
+    num_channels=1,
+    num_kernels=64,
+    kernel_size=(3, 3),
+    padding=(1, 1),
+    activation="relu",
+    frame_size=(64, 64),
+    num_layers=3,
+).to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+criterion = nn.BCELoss(reduction="sum")
